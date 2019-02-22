68ffcbbfa0e30c5b69129f939d845edd7a6889c5
ecupaio <ed.cupaioli@arcadiapower.com>
2019-02-22 10:44:39 -0500

    setup grunt processes

---
+++ gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      options: {
+        style: 'compressed',
+        sourcemap: 'none'
+      },
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'scss',
+          src: ['*.scss'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    },
+    postcss: {
+      options: {
+        processors: [
+          require('pixrem')(),
+          require('autoprefixer')({
+            browsers: 'last 2 versions'
+          }),
+          require('cssnano')()
+        ]
+      },
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    },
+    uglify: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: "js/",
+          src: ["*.js", "!*.min.js"],
+          dest: "js/min/",
+          ext: ".min.js"
+        }]
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '*',
+          livereload: true
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      uglify: {
+        files: ['js/src/*.js'],
+        tasks: ['newer:uglify:target']
+      },
+      sass: {
+        files: ['scss/*.scss'],
+        tasks: ['newer:sass:target']
+      },
+      postcss: {
+        files: ['css/*.css'],
+        tasks: ['newer:postcss:target']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('default', ['uglify', 'sass', 'postcss','connect' ,'watch']);
+};
